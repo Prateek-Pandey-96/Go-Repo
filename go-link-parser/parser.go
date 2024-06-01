4d6d6dc5b538c87b237aba2ad1e90ff876690aa0
prateekpandey <prateekpandey9928@gmail.com>
2024-06-02 00:22:39 +0530

    Removed leading and trailing spaces in link-parser

--- go-link-parser/parser.go
+++ go-link-parser/parser.go
@@ -2,26 +2,27 @@ package golinkparser
 
 import (
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
-type link struct {
+type Link struct {
 	Href string
 	Text string
 }
 
-func Parse(r io.Reader) ([]link, error) {
+func Parse(r io.Reader) ([]Link, error) {
 	doc, err := html.Parse(r)
 	if err != nil {
 		return nil, err
 	}
-	links := []link{}
+	links := []Link{}
 	dfs(doc, &links)
 	return links, nil
 }
 
-func dfs(node *html.Node, links *[]link) {
+func dfs(node *html.Node, links *[]Link) {
 	if node.Type == html.ElementNode && node.Data == "a" {
 		var attrValue string
 		for _, attr := range node.Attr {
@@ -30,7 +31,7 @@ func dfs(node *html.Node, links *[]link) {
 				break
 			}
 		}
-		*links = append(*links, link{Href: attrValue, Text: node.FirstChild.Data})
+		*links = append(*links, Link{Href: attrValue, Text: strings.TrimSpace(node.FirstChild.Data)})
 	}
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
 		dfs(c, links)
