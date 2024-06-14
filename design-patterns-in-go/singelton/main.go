57d3159d0982ee701f10a22f19be25f076a38ef1
prateekpandey <prateekpandey9928@gmail.com>
2024-06-14 22:16:32 +0530

    Singelton Pattern

---
+++ design-patterns-in-go/singelton/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type config struct{}
+
+var mutex *sync.Mutex = &sync.Mutex{}
+var configInstance *config = nil
+var counter int = 1
+
+func GetConfig() {
+	if configInstance == nil {
+		mutex.Lock()
+		defer mutex.Unlock()
+		if configInstance == nil {
+			fmt.Println("Config instance created - ", counter)
+			configInstance = &config{}
+			counter = counter + 1
+		} else {
+			fmt.Println("Config instance already created Condition-1! Returning that one")
+		}
+	} else {
+		fmt.Println("Config instance already created Condition-2! Returning that one")
+	}
+}
+
+func main() {
+	for i := 0; i < 100; i++ {
+		go GetConfig()
+	}
+	fmt.Scanln()
+}
